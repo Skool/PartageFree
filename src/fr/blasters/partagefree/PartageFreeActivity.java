f3281225c936443450741dcee87d9e9ab3c10be6
Thomas DU BOYS <thomas@duboys.info>
2012-03-08 00:46:43 +0100

    Hello World

--- src/fr/blasters/partagefree/PartageFreeActivity.java
+++ src/fr/blasters/partagefree/PartageFreeActivity.java
@@ -2,12 +2,18 @@ package fr.blasters.partagefree;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import org.apache.commons.net.ftp.*;
 
 public class PartageFreeActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        TextView tv = new TextView(this);
+        tv.setText("Hello World");
+        
+        setContentView(tv);
     }
 }
