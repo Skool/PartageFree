064d49c227be1e674efe92060d565ff55a500ef1
Thomas DU BOYS <thomas@duboys.info>
2012-03-07 19:37:39 +0100

    renommage du package

---
+++ src/fr/blasters/partagefree/PartageFreeActivity.java
@@ -0,0 +1,15 @@
+package fr.blasters.partagefree;
+
+import fr.blasters.partagefree.R;
+import fr.blasters.partagefree.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PartageFreeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
