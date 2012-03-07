8ce1855a37f6f659c7c540fb03616a4cc42641af
Thomas DU BOYS <thomas@duboys.info>
2012-03-08 00:42:15 +0100

    nouveau projet clean

---
+++ src/fr/blasters/partagefree/PartageFreeActivity.java
@@ -0,0 +1,13 @@
+package fr.blasters.partagefree;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PartageFreeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
