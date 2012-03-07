c8ab9f85ae9bd39625a779e1e389cce6cf509058
Thomas DU BOYS <thomas@duboys.info>
2012-03-07 19:19:55 +0100

    avec un projet Eclipse

---
+++ src/blasters/partagefree/PartageFreeActivity.java
@@ -0,0 +1,13 @@
+package blasters.partagefree;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PartageFreeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
