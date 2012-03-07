f46bf2fcc67c9f60ce1c0e4a346e60fa2037228d
Thomas DU BOYS <thomas@duboys.info>
2012-03-07 23:43:46 +0100

    Hello World et ménage

--- src/fr/blasters/partagefree/PartageFreeActivity.java
+++ src/fr/blasters/partagefree/PartageFreeActivity.java
@@ -1,15 +1,19 @@
 package fr.blasters.partagefree;
 
-import fr.blasters.partagefree.R;
-import fr.blasters.partagefree.R.layout;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PartageFreeActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        TextView tv = new TextView(this);
+        tv.setText("Hello World");
+        
+        setContentView(tv);
+        
     }
 }
